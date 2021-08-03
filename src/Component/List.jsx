bbb4294a690d8fcc2e72e226d4df6a71e4c8f112
MuhammadIbrahim28 <77695024+MuhammadIbrahim28@users.noreply.github.com>
2021-08-03 08:41:50 -0700

    Initial commit

---
+++ src/Component/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Apply</li>
+      <li>Orange</li>
+      <li>Banana</li>
+    </ul>
+  );
+}
+
+export default List;
